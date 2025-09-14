bcf6b213313128e00b2d299d1e3b71289c0e368a
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-09-14 17:53:50 +0900

    feat: 可視化

--- LogitsObservation.py
+++ LogitsObservation.py
@@ -14,23 +14,35 @@ class LogitObserverProcessor(LogitsProcessor):
         self.tokenizer = tokenizer
         self.top_k = top_k
         self.logit_history = []
-        
+        self.generated_tokens = []
+        self.previous_length = 0
+
     def __call__(self, input_ids, scores):
         top_scores, top_indices = torch.topk(scores[0], min(self.top_k, scores.size(1)))
-        
+
         step_data = {
             "tokens": [],
-            "logits": []
+            "logits": [],
+            "selected": None  # 後で実際に生成されたトークンで更新
         }
-        
+
         for i in range(len(top_scores)):
             token_id = top_indices[i].item()
             token_text = self.tokenizer.decode([token_id])
             logit_score = top_scores[i].item()
-            
+
             step_data["tokens"].append(token_text)
             step_data["logits"].append(logit_score)
-        
+
+        if len(input_ids[0]) > self.previous_length and self.previous_length > 0:
+            generated_token_id = input_ids[0][-1].item()
+            generated_token_text = self.tokenizer.decode([generated_token_id])
+
+            if len(self.logit_history) > 0:
+                self.logit_history[-1]["selected"] = generated_token_text
+                self.generated_tokens.append(generated_token_text)
+
+        self.previous_length = len(input_ids[0])
         self.logit_history.append(step_data)
         return scores
 
@@ -73,10 +85,15 @@ def main():
             eos_token_id=tokenizer.eos_token_id,
             logits_processor=[observer]
         )
-    
+
     input_length = inputs['input_ids'].shape[1]
     generated_text = tokenizer.decode(outputs[0][input_length:], skip_special_tokens=True)
-    
+
+    if len(observer.logit_history) > 0:
+        last_token_id = outputs[0][-1].item()
+        last_token_text = tokenizer.decode([last_token_id])
+        observer.logit_history[-1]["selected"] = last_token_text
+
     result = {
         "input": args.user_input,
         "response": generated_text,
